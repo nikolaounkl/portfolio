6a2695c745a504c27ab2379acb1d835da6708138
nikolaounkl <nikolaounkl@gmail.com>
2019-03-30 20:28:52 +0100

    Added contact base netlify  form.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,6 +11,43 @@ export default function Contact() {
                 <div className="w-100">
                     <h2 className="mb-5">Contact</h2>
                     <p>Together we can make our ideas reality!</p>
+                    <form
+                        name="contact"
+                        method="POST"
+                        netlify-honeypot="bot-field"
+                        data-netlify="true"
+                    >
+                        <p class="hidden">
+                            <label>
+                                Don’t fill this out if you're human:{" "}
+                                <input name="bot-field" />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Your Name: <input type="text" name="name" />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Your Email: <input type="email" name="email" />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Your Subject:{" "}
+                                <input type="text" name="subject" />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Message: <textarea name="message" />
+                            </label>
+                        </p>
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
+                    </form>
                 </div>
             </section>
         </div>
