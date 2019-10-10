8f77076faa185341eb2abdc5fba3f242d82f0cd9
nikolaounkl <nikolaounkl@gmail.com>
2019-10-10 17:03:23 +0200

    Updates

---
+++ precache-manifest.ce03aecf2dabeb87613480656de670bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b35e4df11e6d8e6165a06346d3e0b73",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "19f2c276298581f004c6",
+    "url": "/portfolio/static/css/main.bc483c51.chunk.css"
+  },
+  {
+    "revision": "2355669f42e60862c611",
+    "url": "/portfolio/static/js/2.ab529669.chunk.js"
+  },
+  {
+    "revision": "19f2c276298581f004c6",
+    "url": "/portfolio/static/js/main.f41c1fe9.chunk.js"
+  },
+  {
+    "revision": "b5bdeff08abb45b7b45f",
+    "url": "/portfolio/static/js/runtime~main.093dce58.js"
+  }
+]);
